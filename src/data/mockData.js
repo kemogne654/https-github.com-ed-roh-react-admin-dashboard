0ab39361681b54b54c2d7ae585562f7eeac93cc2
GUYPRINCE <145044244+GUYPRINCE@users.noreply.github.com>
2024-02-05 20:57:20 -0800

    Revert "FIRST PULL"

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -3,15 +3,15 @@ import { tokens } from "../theme";
 export const mockDataTeam = [
   {
     id: 1,
-    name: "prince",
-    email: "prince@gmail.com",
+    name: "Jon Snow",
+    email: "jonsnow@gmail.com",
     age: 35,
     phone: "(665)121-5454",
     access: "admin",
   },
   {
     id: 2,
-    name: "aziz",
+    name: "Cersei Lannister",
     email: "cerseilannister@gmail.com",
     age: 42,
     phone: "(421)314-2288",
