dfe4b19d3d4ddafa6fe7c4ff6880bd6ac29c4c54
Guy prince <kemogneprince06@gmail.com>
2024-02-05 20:53:45 -0800

    FIRST PULL

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -3,15 +3,15 @@ import { tokens } from "../theme";
 export const mockDataTeam = [
   {
     id: 1,
-    name: "Jon Snow",
-    email: "jonsnow@gmail.com",
+    name: "prince",
+    email: "prince@gmail.com",
     age: 35,
     phone: "(665)121-5454",
     access: "admin",
   },
   {
     id: 2,
-    name: "Cersei Lannister",
+    name: "aziz",
     email: "cerseilannister@gmail.com",
     age: 42,
     phone: "(421)314-2288",
